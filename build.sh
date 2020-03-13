f40cbca170d34fcd27c6650bf49ca6279d2fe3a6
Wendly Saintil <wspecs@github.com>
2020-03-13 04:02:12 +0000

    add vim and zsh

--- build.sh
+++ build.sh
@@ -1,19 +1,47 @@
 #!/bin/bash
+echo Installing essentials packages...
+
+TMUX_VERSION=57eeffcf0e5b356b9cda27aa44edbf2c849103eb
+OH_MY_ZSH_VERSION=07e3236bc5c8dbf9d818a4f0145f09bdb4bec6f0
+DOTFILES_VERSION=876401856ddbb70cf0ffe58b7f3ebc948eaf9524
 
 if [ ! /etc/wspecs/functions.sh ]; then
   echo wspecs/box-functions is required
-	exit 1
+  exit 1
 fi
 source /etc/wspecs/functions.sh
 
-
 hide_output apt-get update
-
-function install_once() {
-	if which $1 > /dev/null; then
-    echo "Installing $1"
-    hide_output apt-get install $1
-	fi
-}
-
 install_once tmux
+install_once curl
+install_once zsh
+install_once git
+
+echo Adding tmux conf file
+cd $HOME
+git_clone https://github.com/gpakosz/.tmux.git $TMUX_VERSION '' .tmux
+ln -s -f .tmux/.tmux.conf
+cp .tmux/.tmux.conf.local .
+rm -rf .tmux
+
+if [ ! -f .oh-my-zsh/oh-my-zsh.sh ]; then
+  git_clone https://github.com/ohmyzsh/ohmyzsh.git $OH_MY_ZSH_VERSION '' ohmyzsh
+  sh -c ohmyzsh/tools/install.sh <<< $'y\n'
+  chsh -s `which zsh` $USER
+  rm -rf ohmyzsh
+fi
+
+if [ ! -d .vim/autoload ]; then
+  echo installing vim bundles
+  mkdir -p ~/.vim/autoload ~/.vim/bundle
+  curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+  git clone https://github.com/mattn/emmet-vim.git ~/.vim/bundle/emmet-vim
+  git clone https://github.com/preservim/nerdtree.git ~/.vim/bundle/nerdtree
+fi
+
+git_clone https://github.com/wspecs/dotfiles.git $DOTFILES_VERSION '' dotfiles
+cp dotfiles/vim/.vimrc ./
+cp dotfiles/zsh/.zsh* ./
+rm -rd dotfiles
+
+goto_working_directory
